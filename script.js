195b6456c4819db2f25e07e9227bff41ad26ccb1
Keisham Thoisana <keishamthoisana@gmail.com>
2026-02-01 13:48:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ yesBtn.addEventListener('click', () => {
 
 function shareWhatsApp() {
   const text = encodeURIComponent(
-    "💖 Ranju said YES! Happy Valentine’s Day 💘"
+    "💖 Shw said YES! Happy Valentine’s Day 💘"
   )
   window.open(`https://wa.me/?text=${text}`)
 }
