8f46b101c0659ad2e6b131f4290db3209bc0d54e
Thoisana <thoisana.keisham@infosys.com>
2026-02-01 12:23:23 +0530

    Updates

---
+++ _next/static/uFSkD23Gq6L_-TbQh7ZwQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/967-e7ecaff3fc8a9102.js","static/chunks/pages/index-0cfc952e3a8500d3.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
